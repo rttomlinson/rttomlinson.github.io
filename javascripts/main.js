32a68161ee7074d8d8677f4e7a8fb8a8d7c6950a
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-16 14:08:25 +0000

    Add pseudocode to main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,14 @@
 "use strict";
 
-
-$(document).ready(function () {
+$(document).ready(function() {
     console.log("nice");
+    //Get JSON from the heroku_projects pages
+
+    //Target class .container-for-projects
+
+    // <div class="col-md-4 project-holder">
+    //   <a type="button" data-toggle="modal" data-target="#myModal{{@index}}">
+    //     <img class="img-responsive center-block highlightable" src={{project.imageSource}} alt="Placeholder image">
+    //   </a>
+    // </div>
 })
